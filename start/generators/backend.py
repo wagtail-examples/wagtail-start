1957df769708cb397c8a4385a08d23b5c4ab1548
Nick Moreton <nick.moreton@torchbox.com>
2023-11-02 22:43:18 +0000

    Move movers and settings

--- start/generators/backend.py
+++ start/generators/backend.py
@@ -2,9 +2,7 @@ import subprocess
 
 import click
 
-from start.processors.movers import move_files_settings
 from start.processors.readme import generate_readme
-from start.processors.settings import update_base_settings, update_urls
 
 
 def generate_backend(path_manager):
@@ -76,3 +74,122 @@ def replace_home_page(path_manager):
         "w",
     ) as f:
         f.write(home_page)
+
+
+def move_files_settings(path_manager):
+    subprocess.run(  # SETTINGS
+        [
+            "mv",
+            str(path_manager.package_path / path_manager.package_name / "settings"),
+            str(path_manager.package_path),
+        ],
+        check=True,
+    )
+
+    subprocess.run(  # URLS
+        [
+            "mv",
+            str(path_manager.package_path / path_manager.package_name / "urls.py"),
+            str(path_manager.package_path),
+        ],
+        check=True,
+    )
+
+    subprocess.run(  # WSGI
+        [
+            "mv",
+            str(path_manager.package_path / path_manager.package_name / "wsgi.py"),
+            str(path_manager.package_path),
+        ],
+        check=True,
+    )
+
+    subprocess.run(  # STATIC
+        [
+            "mv",
+            str(path_manager.package_path / path_manager.package_name / "static"),
+            str(path_manager.package_path),
+        ],
+        check=True,
+    )
+
+    subprocess.run(  # TEMPLATES
+        [
+            "mv",
+            str(path_manager.package_path / path_manager.package_name / "templates"),
+            str(path_manager.package_path),
+        ],
+        check=True,
+    )
+
+    subprocess.run(  # INIT
+        [
+            "mv",
+            str(path_manager.package_path / path_manager.package_name / "__init__.py"),
+            str(path_manager.package_path),
+        ],
+        check=True,
+    )
+
+    subprocess.run(
+        ["rm", "-rf", str(path_manager.package_path / path_manager.package_name)],
+        check=True,
+    )  # FOLDER
+
+    subprocess.run(
+        ["mv", str(path_manager.package_path / "manage.py"), path_manager.project_path],
+        check=True,
+    )  # MANAGE
+
+    subprocess.run(
+        [
+            "mv",
+            str(path_manager.package_path / ".dockerignore"),
+            path_manager.project_path,
+        ],
+        check=True,
+    )  # DOCKERIGNORE
+
+    subprocess.run(
+        [
+            "mv",
+            str(path_manager.package_path / "Dockerfile"),
+            path_manager.project_path,
+        ],
+        check=True,
+    )  # DOCKERFILE
+
+    subprocess.run(
+        [
+            "mv",
+            str(path_manager.package_path / "requirements.txt"),
+            path_manager.project_path,
+        ],
+        check=True,
+    )  # REQUIREMENTS
+
+
+def update_urls(path_manager):
+    old = "from search import views as search_views"
+    new = f"from {path_manager.package_name}.search import views as search_views"
+
+    with open(path_manager.package_path / "urls.py", "r") as f:
+        content = f.read()
+        content = content.replace(old, new)
+
+    with open(path_manager.package_path / "urls.py", "w") as f:
+        f.write(content)
+
+
+def update_base_settings(path_manager):
+    with open(path_manager.package_path / "settings" / "base.py", "r") as f:
+        content = f.read()
+        # Double quotes used by 3.0+
+        content = content.replace('"home",', f'"{path_manager.package_name}.home",')
+        content = content.replace('"search",', f'"{path_manager.package_name}.search",')
+        # Single quotes used up to 2.16
+        content = content.replace("'home',", f"'{path_manager.package_name}.home',")
+        content = content.replace("'search',", f"'{path_manager.package_name}.search',")
+
+    with open(path_manager.package_path / "settings" / "base.py", "w") as f:
+        f.write(content)
